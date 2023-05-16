c42bbd1ac22c7a5fdf39a809e4c1ee3a3bb3b562
Mihir Pandya <mihir.m.pandya@gmail.com>
2023-05-16 12:26:37 -0700

    stupid stuff

--- dashapp/dashapp.py
+++ dashapp/dashapp.py
@@ -1,17 +1,24 @@
+from typing import List
+
 import dash
-import flask
 import pandas as pd
 from dash import dcc, html
 from dash.dependencies import Input, Output
+from sqlalchemy.orm import Session
+
+from main import get_db
+from models.event import Event
 
 
-def create_dash_app(requests_pathname_prefix: str = None, secret: str = None) -> dash.Dash:
-    server = flask.Flask(__name__)
-    server.secret_key = secret
+# select date_format(event_timestamp,'%Y-%m-%d %H'), count(1) from events group by 1;
 
+
+def create_dash_app(requests_pathname_prefix: str = None) -> dash.Dash:
+    db: Session = get_db()
+    event_types: List[str] = db.query(Event).distinct(Event.event_type).all()
     df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/hello-world-stock.csv')
 
-    app = dash.Dash(__name__, server=server, requests_pathname_prefix=requests_pathname_prefix)
+    app = dash.Dash(__name__, requests_pathname_prefix=requests_pathname_prefix)
 
     app.scripts.config.serve_locally = False
     dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-basic-latest.min.js'
@@ -20,12 +27,8 @@ def create_dash_app(requests_pathname_prefix: str = None, secret: str = None) ->
         html.H1('Stock Tickers'),
         dcc.Dropdown(
             id='my-dropdown',
-            options=[
-                {'label': 'Tesla', 'value': 'TSLA'},
-                {'label': 'Apple', 'value': 'AAPL'},
-                {'label': 'Coke', 'value': 'COKE'}
-            ],
-            value='TSLA'
+            options=[{'label': x, 'value': x} for x in event_types],
+            value=event_types[0]
         ),
         dcc.Graph(id='my-graph')
     ], className="container")
