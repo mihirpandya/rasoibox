d7075c82b3288dfff76aaf87f36a48f7e79a2422
Mihir Pandya <mihir.m.pandya@gmail.com>
2023-07-12 18:27:57 -0700

    fix email

--- api/price.py
+++ api/price.py
@@ -1,4 +1,5 @@
 import logging
+import re
 from typing import Optional, List
 
 from pydantic import BaseModel, validator, ValidationError
@@ -22,8 +23,31 @@ class RecipeServingPrice(BaseModel):
 class ReferredEmails(BaseModel):
     referred_emails: List[str]
 
+    @validator('referred_emails')
+    def validate_referred_emails(cls, referred_emails: List[str]) -> List[str]:
+        pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        clean_emails: List[str] = []
+        for email in referred_emails:
+            if bool(re.match(pattern, email)):
+                clean_email = email.lower()
+                clean_email = clean_email.strip()
+                clean_emails.append(clean_email)
+            logger.error("Malformed email address: %s", email)
+            raise ValidationError("Malformed email address.")
+        return clean_emails
+
 
 class Invitation(BaseModel):
     referred_emails: ReferredEmails
     referrer_email: str
     referrer_verification_code: Optional[str]
+
+    @validator('referrer_email')
+    def validate_referrer_email(cls, referrer_email: str) -> str:
+        pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        if bool(re.match(pattern, referrer_email)):
+            clean_email = referrer_email.lower()
+            clean_email = clean_email.strip()
+            return clean_email
+        logger.error("Malformed email address: %s", referrer_email)
+        raise ValidationError("Malformed email address.")
