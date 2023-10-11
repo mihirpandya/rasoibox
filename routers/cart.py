dbaeb8a41bb56470308e022717a92846bc44ccc6
Mihir Pandya <mihir.m.pandya@gmail.com>
2023-10-11 16:25:38 -0700

    small fix

--- routers/cart.py
+++ routers/cart.py
@@ -28,6 +28,7 @@ async def get_active_carts(db: Session = Depends(get_db)):
     emailable_code: Dict[str, str] = {}
     for cart_item in all_cart_items:
         email: Optional[str] = None
+
         if cart_item.verification_code in emailable_code.keys():
             email = emailable_code[cart_item.verification_code]
         else:
@@ -35,16 +36,19 @@ async def get_active_carts(db: Session = Depends(get_db)):
                 VerifiedSignUp.verification_code == cart_item.verification_code).first()
             if verified_sign_up is not None:
                 email = verified_sign_up.email
+                emailable_code[cart_item.verification_code] = verified_sign_up.email
             else:
                 unverified_sign_up = db.query(UnverifiedSignUp).filter(
                     UnverifiedSignUp.verification_code == cart_item.verification_code).first()
                 if unverified_sign_up is not None:
                     email = unverified_sign_up.email
+                    emailable_code[cart_item.verification_code] = unverified_sign_up.email
 
         if email is not None:
             items: List[int] = []
             if email in result.keys():
                 items = result[email]
             items.append(cart_item.recipe_id)
+            result[email] = items
 
     return JSONResponse(jsonable_encoder(result))
